7b7329b712581061dfcc401ebe81c9d74b3a2312
Aaryan <aaryan359500@gmail.com>
2021-06-21 21:48:36 +0530

    1. frontend begin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Web3 from "web3";
+import MetaMaskGuide from "./pages/MetaMaskGuide";
+import Admin from "./abis/Admin.json";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    isMetaMaskPresent: true,
+  };
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockChainData();
+  }
+
+  async loadWeb3() {
+    this.setState({ isMetaMaskPresent: true });
+    if (window.ethereum) {
+      window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      window.web3 = new Web3(window.ethereum);
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      this.setState({ isMetaMaskPresent: false });
+    }
+  }
+
+  loadBlockChainData = async () => {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    const networkId = await web3.eth.net.getId();
+    const AdminData = await Admin.networks[networkId];
+    if (AdminData) {
+      const admin = await new web3.eth.Contract(Admin.abi, AdminData.address);
+      const owner = await admin.methods.owner().call();
+      console.log(owner);
+    } else {
+      toast.error("The Admin Contract does not exist on this network!");
+    }
+  };
+
+  render() {
+    return this.state.isMetaMaskPresent ? (
+      <div>
+        <ToastContainer />
+        Hi
+      </div>
+    ) : (
+      <MetaMaskGuide />
+    );
+  }
 }
-
-export default App;
